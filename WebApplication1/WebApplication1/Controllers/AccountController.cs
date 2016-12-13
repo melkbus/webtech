2061ef08413402ba9fb3a589c13ed0313661a156
Jens Cardon <Jens Cardon@DESKTOP-HR2TUIM>
2016-12-13 21:20:25 +0100

    louche lijnen eruit

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -154,18 +154,15 @@ namespace WebApplication1.Controllers
         [AllowAnonymous]
         public ActionResult Profile(int id)
         {
-            
-<<<<<<< HEAD
             webtechEntities db = new webtechEntities();
             account user = new account();
             user = db.account.Find(id);
-=======
-            account user = db.account.Find(id);
+
+            account usr = db.account.Find(id);
             Event[] t = db.Event.ToArray();
             ViewBag.EventsMade = t;
             ViewBag.EventsParticipated = t;
->>>>>>> 42828337cb5005f640b48387d34ad3f53039a4e3
-            return View(user);
+            return View(usr);
             }
 
         [AllowAnonymous]
